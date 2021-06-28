4ff5dbe24c5a50c5795a8488eab5d0168dd2ec71
panyf <panyufang1995@qq.com>
2021-06-28 14:12:59 +0800

    负载均衡

--- cloud-consumer-order80/src/main/java/com/xiaofang/springcloud/CloudConsumerOrder80Application.java
+++ cloud-consumer-order80/src/main/java/com/xiaofang/springcloud/CloudConsumerOrder80Application.java
@@ -1,11 +1,14 @@
 package com.xiaofang.springcloud;
 
+import com.xiaofang.rule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name="CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class CloudConsumerOrder80Application {
     public static void main(String[] args) {
         SpringApplication.run(CloudConsumerOrder80Application.class, args);
